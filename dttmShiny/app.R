3f58e63fc1d3e2fdbade6a657ce62afec5b7b4d7
Mark Culp <markculp0@gmail.com>
2017-09-04 10:53:32 -0500

    add clock time adjustments; honolulu and los angeles tz calc

--- dttmShiny/app.R
+++ dttmShiny/app.R
@@ -7,8 +7,9 @@
 library(shiny)
 library(lubridate)
 
-
-timeVal <- ""
+myTZ <- ""          # Current local computer TZ
+timeVal <- ""       # For calculations, 1-24
+myClockTime <- ""   # For display times, 1 - 12
 
 # Define UI for application
 ui <- fluidPage(
@@ -35,8 +36,10 @@ ui <- fluidPage(
       
       # Main Panel GUI
       mainPanel(
-        
-        textOutput("yourTime"),
+        textOutput("locHonoluTZ"),
+        textOutput("locHonoluTime"),
+        textOutput("locLosAngTZ"),
+        textOutput("locLosAngTime"),
         plotOutput("distPlot")
       )
    )
@@ -51,22 +54,70 @@ server <- function(input, output) {
   # Print my TZ
   output$myTzOut <- renderText(myTZ) 
   
-  # Get time value from slider function
-  mydttm<- reactive({
+  # Get time value from slider function and display it
+  my_tm<- reactive({
     timeVal <- input$sldTime
-    timeVal <- paste(timeVal, ":00")
+    if (timeVal > 12) {
+      myClockTime <- timeVal - 12
+      # Adjust for 12 noon
+      if (myClockTime == 12) {paste("  ", myClockTime, ":00 am")
+      } else { paste("  ", myClockTime, ":00 pm") }
+    } else {
+      myClockTime <- timeVal
+      # Adjust for 12 midnight
+      if (myClockTime == 12){paste("  ", myClockTime, ":00 pm")
+      } else { paste("  ", myClockTime, ":00 am") }
+    }
+  })
+  
+  # Get and display Honolulu dttm
+  honolu_dttm <- reactive({
+    # Adjust time for single digit hours
+    if (input$sldTime > 9){
+      tm <- paste(input$sldTime, "00", sep = "")
+    } else {
+      tm <- paste("0", input$sldTime, "00", sep = "")
+    }
+    dttm <- paste(input$meetDate, tm, sep = " ")
+    dttm <- ymd_hm(dttm, tz = myTZ)
+    dttm <- with_tz(dttm, tz = "Pacific/Honolulu")
+    paste(format(dttm,"%Y-%m-%d %H:%M"))
+  })
+  
+  # Get and display Los Angeles dttm
+  losAng_dttm <- reactive({
+    # Adjust time for single digit hours
+    if (input$sldTime > 9){
+      tm <- paste(input$sldTime, "00", sep = "")
+    } else {
+      tm <- paste("0", input$sldTime, "00", sep = "")
+    }
+    dttm <- paste(input$meetDate, tm, sep = " ")
+    dttm <- ymd_hm(dttm, tz = myTZ)
+    dttm <- with_tz(dttm, tz = "America/Los_Angeles")
+    paste(format(dttm,"%Y-%m-%d %H:%M"))
   })
   
   # Output to my local time (Side Panel)
   output$myTime = renderText({
-    mydttm()
+    my_tm()
   })  
   
-  # Output to other local time (Main Panel)
-  output$yourTime = renderText({
-    mydttm()
+  # Output Honolulu local time zone (Main Panel)
+  output$locHonoluTZ <- renderText("Pacific/Honolulu")
+    
+  # Output to Honolulu local time (Main Panel)
+  output$locHonoluTime = renderText({
+    honolu_dttm()
   })
   
+  # Output Los Angeles local time zone (Main Panel)
+  output$locLosAngTZ <- renderText("America/Los_Angeles")
+  
+  # Output to Los Angeles local time (Main Panel)
+  output$locLosAngTime = renderText({
+    losAng_dttm()
+  })
   
   # Plot, maybe?
   output$distPlot <- renderPlot({
